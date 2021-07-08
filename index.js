cc7c82e329306a30ca4ce36a0034e2338ab8288e
Jasef21 <81518170+Jasef21@users.noreply.github.com>
2021-07-08 01:17:23 -0500

    barra responsive lista
    al fin salio :''v

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".navegador");
+const navMenu = document.querySelector(".nav-menu");
 
 navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("nav-menu_visible");

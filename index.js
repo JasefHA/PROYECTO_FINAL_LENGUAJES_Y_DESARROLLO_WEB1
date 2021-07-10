d3e577117521873c47a422fec13fc2c9a3481726
Jasef21 <81518170+Jasef21@users.noreply.github.com>
2021-07-10 16:56:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,4 +20,18 @@ const btnSwitch = document.querySelector("#switch");
 btnSwitch.addEventListener("click",()=>{
   document.body.classList.toggle("dark");
   btnSwitch.classList.toggle("active");
+
+  // local storage
+  if(document.body.classList.contains("dark")){
+      localStorage.setItem("dark-mode", "true");
+  } else{
+    localStorage.setItem("dark-mode", "false");
+  }
 })
+
+  // Obtener modo actual
+  if(localStorage.getItem("dark-mode")=="true"){
+    document.body.classList.add("dark");
+  }else{
+    document.body.classList.remove("dark");
+  }

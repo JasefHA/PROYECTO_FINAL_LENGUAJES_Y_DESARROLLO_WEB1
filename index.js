f532a67874df637f0249a7aa0927c494c2bf2e9f
Jasef21 <81518170+Jasef21@users.noreply.github.com>
2021-07-10 22:48:51 -0500

    chau cookies

--- index.js
+++ index.js
@@ -39,22 +39,4 @@ btnSwitch.addEventListener("click",()=>{
   }
 
 /*COOKIES */
-  const botonAceptarCookies = document.getElementById("boton-cookie");
-  const avisoCookies = document.getElementById("aviso-cookies");
-  const fondoAvisoCookies = document.getElementById("fondo-aviso-cookies");
-
-  // dataLayer = [];
-
-  // if(!localStorage.getItem("cookies-aceptadas")){
-    
-  // }
-
-  avisoCookies.classList.add("activo");
-    fondoAvisoCookies.classList.add("activo");
-
-  botonAceptarCookies.addEventListener("click", () => {
-    avisoCookies.classList.remove("activo");
-    fondoAvisoCookies.classList.remove("activo");
-
-    localStorage.setItem("cookies-aceptadas", true);
-  })
+  

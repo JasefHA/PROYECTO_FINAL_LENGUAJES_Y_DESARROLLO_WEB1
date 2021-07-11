c25f67171dea5aea952d5a32bb832d8d1c01a17d
Jasef21 <81518170+Jasef21@users.noreply.github.com>
2021-07-11 12:35:47 -0500

    gg

--- aviso-cookies.js
+++ aviso-cookies.js
@@ -4,7 +4,3 @@ const fondoAvisoCookies = document.getElementById("fondo-aviso-cookies");
 
 
 
-botonAceptarCookies.addEventListener('click',()=>{
-    avisoCookies.classList.remove('activo');
-    fondoAvisoCookies.classList.remove('activo');
-});
